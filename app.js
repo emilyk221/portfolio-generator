b6d62347134495038c28620d7c7bfcf633801914
emilyk <emily.g.kruse@gmail.com>
2022-04-02 15:28:43 -0400

    create promises

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
 const generatePage = require("./src/page-template.js");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -182,16 +182,21 @@ const mockData = {
   ]
 };
 
-// promptUser()
-//   .then(promptProject)
-//   .then(portfolioData => {
-//     const pageHTML = generatePage(portfolioData);
-const pageHTML = generatePage(mockData);
-    fs.writeFile("./index.html", pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log("Page created! Check out index.html in this directory to see it!");
-    });
-  // });
-
-
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
